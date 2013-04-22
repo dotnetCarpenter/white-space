0098b96ba810a89ed9a22d387bf799338548fb0f
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-04-22 19:07:27 +0200

    playing around with a queue monad

--- white-space.js
+++ white-space.js
@@ -15,9 +15,54 @@
 	var empty = /\s+/;
 	var stylesheets = doc.styleSheets;
 	var stop = null;
-	
-	iterator.call(stylesheets, getStyleSheet);
+	var queue = function() {
+		var args = getArguments.call(arguments);
+		return (function() {
+			var currentFn = args.shift();
+			this.length = args.length;
+			currentFn.apply(currentFn, getArguments.call(arguments));
+		}).apply(args);
+	}
+	queue(getStyleSheet, ajax, parseCss, removeWhiteSpace);
 
+	//iterator.call(stylesheets, getStyleSheet);
+	function getArguments() {
+		return Array.prototype.splice.call(this, 0, this.length);
+	}
+
+	function iterator(fn) {
+		for (var i = 0, len = this.length; i < len; ++i) {
+			if( fn.call(this, this[i], i) === stop ) return;
+		}
+	}
+	function getStyleSheet(sheet) {
+		//console.dir(sheet);
+		ajax(sheet.href, parseCss, removeWhiteSpace);
+	}
+	function ajax(url, callback) {
+		var args = [].splice.call(arguments, 2, arguments.length);
+		var get = new win.XMLHttpRequest();
+		get.open('GET', url);
+		get.onreadystatechange = function() {
+			if ( this.readyState !== 4 || this.status !== 200 && this.status !== 304 && this.status !== 0 ){
+				return;
+			}
+			callback.apply(callback, [this.responseText].concat(args));
+		}
+		get.send();
+	}
+	function parseCss(css, callback) {
+		//console.log(css);
+		var tokens = css.match(cssTokenizer);
+		var matches = [];
+		iterator.call(tokens, function(cssblock) {
+			if( isWhiteSpaceCssBlock.test(cssblock) ) {
+				//console.log(cssblock);
+				matches.push(cssblock.match(cssSelector)[1]);
+			}
+		});
+		callback.apply(callback, matches);
+	}
 	function removeWhiteSpace() {
 		//console.dir(arguments);
 		iterator.call(arguments, function(selector) {
@@ -34,38 +79,5 @@
 					}			
 				});
 		});
-	}
-	function parseCss(css, callback) {
-		//console.log(css);
-		var tokens = css.match(cssTokenizer);
-		var matches = [];
-		iterator.call(tokens, function(cssblock) {
-			if( isWhiteSpaceCssBlock.test(cssblock) ) {
-				//console.log(cssblock);
-				matches.push(cssblock.match(cssSelector)[1]);
-			}
-		});
-		callback.apply(callback, matches);
-	}
-	function getStyleSheet(sheet) {
-		//console.dir(sheet);
-		ajax(sheet.href, parseCss, removeWhiteSpace);
-	}
-	function iterator(fn) {
-		for (var i = 0, len = this.length; i < len; ++i) {
-			if( fn.call(this, this[i], i) === stop ) return;
-		}
-	}
-	function ajax(url, callback) {
-		var args = [].splice.call(arguments, 2, arguments.length);
-		var get = new win.XMLHttpRequest();
-		get.open('GET', url);
-		get.onreadystatechange = function() {
-			if ( this.readyState !== 4 || this.status !== 200 && this.status !== 304 && this.status !== 0 ){
-				return;
-			}
-			callback.apply(callback, [this.responseText].concat(args));
-		}
-		get.send();
 	}
 })(document, window);
