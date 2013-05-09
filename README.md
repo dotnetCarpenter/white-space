d2380f9cfd89fe8938a9ec28cb6d8a5f605a972c
dotnetCarpenter <jon.ronnenberg@gmail.com>
2013-05-09 14:32:40 +0200

    removed timing instrumentation and version bump

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ So far there is three test cases and one test case that include all three:
 + http://lists.w3.org/Archives/Public/www-style/2013Apr/subject.html#msg497
 
 ### Changelog
++ 0.1.2 - fix bug when no white-space:none was found in CSS
 + 0.1.1 - switch to monad implementation
 + 0.1.0 - proof of concept
 
--- white-space.js
+++ white-space.js
@@ -5,7 +5,6 @@
  * @version  13.4.2 	year.month.minor-version
  * Might have an issue with the DOM not being ready before removing white space.
  */
-var startTime = performance.now();
 ;(function whiteSpace(doc, win) {
 	"use strict";
 
@@ -34,7 +33,6 @@ var startTime = performance.now();
 					}			
 				});
 		});
-		console.log("Time:", performance.now()-startTime);
 	}
 	function parseCss(css, callback) {
 		//console.log(css);
